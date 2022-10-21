bfd7276605d722869c516eaf42524a2479c10b72
sdwoo <damjang67@naver.com>
2022-10-21 17:02:11 +0900

    [level 2] Title: 가장 큰 수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+
+    numbers.sort((a,b) => {
+        const compareA = parseInt(a.toString() + b.toString());
+        const compareB = parseInt(b.toString() + a.toString());
+        return compareB - compareA
+    })
+
+    if(numbers.join('')[0] === '0') return '0' // 0000인 경우 뺴기
+    
+    return numbers.join('');
+}
+
