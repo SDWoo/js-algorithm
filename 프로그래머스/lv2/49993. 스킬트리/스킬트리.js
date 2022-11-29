c5a9876b74585f9d1c4203f1b6396c2a54831cfe
sdwoo <damjang67@naver.com>
2022-11-30 00:56:53 +0900

    [level 2] Title: 스킬트리, Time: 3.43 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.js
@@ -1,45 +1,31 @@
 function solution(skill, skill_trees) {
-
-  order = skill;
-  tree  = skill_trees;
-
-  // tree에서 order에 해당하는 값만 남겨두기
-  // *** 문제에서 중복 값이 없다고 하여 중복값 제거 코드는 없음
-  for(let t=0; t<tree.length; t++){ // tree
-    let temp = [];
-    for(let s=0; s<tree[t].length; s++){ // singles
-      let isOrder = order.includes(tree[t][s]);
-      if(isOrder){
-        temp.push(tree[t][s])
-      }
-    }
-    tree[t]=temp.join('');
-  }
-
-
-  // 순서 배열 생성하기
-  let arr = [];
-  for(let t=0; t<tree.length; t++){
-    let temp = [];
-    for(let s=0; s<tree[t].length; s++){
-      temp.push(order.indexOf(tree[t][s]));
-    }
-    arr.push(temp)
-  }
-
-  // [ [ 1, 0, 2 ], [ 0, 1, 2 ], [ 0, 1 ], [ 1, 2 ] ]
-  // 만들어진 순서 배열이 0부터 순차적인 값을 지니고 있어야 true
-  let isRightOrder = new Array(arr.length).fill(true)
-  for(let i=0; i<arr.length; i++){
-    let k = 0;
-    for(let j=0; j<arr[i].length; j++){
-      if(arr[i][j]!==k){
-        isRightOrder[i]=false;
-        continue;
-      }
-      k++;
-    }
-  }
-  const answer = isRightOrder.filter(bool => bool==true).length
-  return answer;
+    let result = 0;
+    skill_trees.forEach((tree, index) => {
+        const temp = [];
+        tree.split('').forEach((element) => {
+            if(skill.indexOf(element) > -1 ){
+                temp.push(element);
+            }
+        })
+        skill_trees[index] = temp;
+    })
+    console.log(skill_trees);
+    skill_trees.forEach((tree) => {
+        let isOk = true;
+        for (let i = 0; i < tree.length; i++) {
+            if(skill.indexOf(tree[i]) !== i) {
+                isOk = !isOk
+                break;
+            }    
+        }
+        // tree.forEach((value, index) => {
+        //     if(skill.indexOf(value) !== index) {
+        //         isOk = !isOk
+        //         return false;
+        //     }
+        // })
+        result = isOk ? result +1 : result;
+    })
+    
+    return result;
 }
