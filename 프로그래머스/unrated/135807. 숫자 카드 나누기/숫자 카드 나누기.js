a1d77d4c1305a76d0849a2ff800b853ac1958779
sdwoo <damjang67@naver.com>
2022-12-12 23:28:50 +0900

    [unrated] Title: 숫자 카드 나누기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -0,0 +1,15 @@
+function solution(arrayA, arrayB) {
+    let a = getDvsr(arrayA, arrayB);
+    let b = getDvsr(arrayB, arrayA);
+        
+    return Math.max(a,b);
+}
+function getDvsr(arrayA, arrayB){
+    let answer = 0;
+    const gcd =  (a, b) => a % b === 0 ? b : gcd(b, a % b);
+    
+    arrayA.forEach(val => answer = gcd(answer, val));
+    if(arrayB.some(val => val%answer === 0)) return 0;
+    
+    return answer;
+}
