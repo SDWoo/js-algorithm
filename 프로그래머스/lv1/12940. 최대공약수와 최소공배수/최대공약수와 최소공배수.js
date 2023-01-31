e1058f6df484d74a66ebc766ab1618bc259d3764
sdwoo <damjang67@naver.com>
2023-01-31 20:40:20 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,8 @@
+function solution(n, m) {
+    const gcb = (a,b) => {
+        if(b===0) return a;
+        return gcb(b,a%b);
+    }
+    const lcm = (a,b) => (a*b) / gcb(a,b);
+    return [gcb(n,m), lcm(n,m)]
+}
