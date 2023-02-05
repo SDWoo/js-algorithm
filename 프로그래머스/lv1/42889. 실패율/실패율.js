4ba49be5c4c2b162fa6433a7546e196851968402
sdwoo <damjang67@naver.com>
2023-02-05 21:07:37 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -0,0 +1,10 @@
+function solution(N, stages) {
+    let answer= [];
+    let person = stages.length;
+    for (let i =1; i<= N; i ++) {
+        answer.push([i, stages.filter(n => n === i).length / person]);
+        person -= stages.filter(n =>  n === i).length;
+    }
+    answer = answer.sort((a,b) => b[1] - a[1]).map( a=> a[0]);
+    return answer;
+}
