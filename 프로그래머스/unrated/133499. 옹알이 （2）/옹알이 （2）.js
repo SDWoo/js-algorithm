5e3b180bb57108833adcff226316a42d03f09900
sdwoo <damjang67@naver.com>
2023-02-16 23:55:28 +0900

    [unrated] Title: 옹알이 (2), Time: 0.61 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -2,9 +2,6 @@ function solution(babbling) {
     const list = ['ma','woo','ye','aya'];
     let result = 0;
     
-    // 받아온 배열의 value를 하나씩 검사
-    // 일단 옹알이 리스트에 있는 것들로 이루어진다 => result + 1;
-    // 옹알이 리스트 하나씩 검사. 
     babbling.forEach((value) => {
         let i = 0;
         let count = 0;
@@ -15,16 +12,11 @@ function solution(babbling) {
                i = list.length;
             }
             
-            if(matchValue) {
-                count += matchValue.length;
-
-            }
+            count = matchValue? count + matchValue.length : count;
             
         })
         
         while(i < list.length) {
-
-            
             if(value.includes(list[i])) {
                 value = value.replace(list[i],'')
                 count -= 1;
