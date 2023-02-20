86e307f7855399ddd814bd145df7a031ba730532
sdwoo <damjang67@naver.com>
2023-02-20 23:50:24 +0900

    [unrated] Title: 문자열 나누기, Time: 1.06 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/README.md
+++ 프로그래머스/unrated/140108. 문자열 나누기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 문자열 나누기 - 140108 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140108#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140108) 
 
 ### 성능 요약
 
-메모리: 37 MB, 시간: 1.19 ms
+메모리: 36.6 MB, 시간: 1.06 ms
 
 ### 구분
 
--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
@@ -1,10 +1,11 @@
 function solution(s) {
-    let answer = 0;
-    const map = new Map();
     if(s.length === 1) return 1;
+    
+    let answer = 0;
     let first = s[0];
     let firstNum = 0;
     let otherNum = 0;
+    
     s.split('').forEach((char,index) => {
         if(char === first) {
             firstNum += 1;
@@ -13,24 +14,11 @@ function solution(s) {
         }
         
         if (firstNum === otherNum) {
-            first = index === s.lengt - 1? '' : s[index+1];
+            first = index === s.length - 1 ? '' : s[index+1];
             firstNum = 0;
             otherNum = 0;
             answer += 1;
         }
-        
-//         const plusnum = map.get(char) ? map.get(char) + 1 : 1;
-//         const array = [...map.values()];
-                
-//         if(index > 0 && array.indexOf(plusnum) > -1){
-//             [...map.keys()].forEach((key) => {
-//                 map.delete(key)
-//             })
-//             answer += 1;
-//         }
-//         else {
-//             map.set(char, plusnum);
-//         }
         
     })
     
