d2a90678aae8ec3e41bf428722a0e7bef2ef3968
sdwoo <damjang67@naver.com>
2023-03-24 17:23:59 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,17 +1,3 @@
 function solution(A, B) {
-    if(A===B) return 0;
-    
-    let count = 0;
-    
-    while(count < A.length-1) {
-        A = A.split('');
-        const last = A.pop();
-        A = last + A.join('');
-        count += 1;
-        
-        if (A === B) return count;
-        
-    }
-    
-    return -1;
+    return (B+B).indexOf(A);
 }
