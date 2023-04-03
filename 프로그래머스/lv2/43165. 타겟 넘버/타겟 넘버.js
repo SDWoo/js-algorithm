243537fad1aa6ed9cf89a6f3657af5d0ce448e63
sdwoo <damjang67@naver.com>
2023-04-03 15:19:25 +0900

    [level 2] Title: 타겟 넘버, Time: 1.44 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -3,18 +3,17 @@ function solution(numbers, target) {
     
     const dfs = (index, currentAnswer, sign) => {
         currentAnswer += sign * numbers[index];
-        
         if(index < numbers.length - 1) {
             dfs(index + 1, currentAnswer, -1);
             dfs(index + 1, currentAnswer, 1);            
         }
         if(index === numbers.length-1 && currentAnswer === target) {
             answer += 1;
+            return;
         }
     }
     dfs(0, 0, -1);
     dfs(0, 0, 1);
-        
     
     return answer;
 }
