d243b21fc2ca8ea554ee175dec4449aa3b3c220e
sdwoo <damjang67@naver.com>
2023-04-27 10:44:17 +0900

    [level 1] Title: 체육복, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,31 +1,26 @@
 function solution (n, lost, reserve) {
-    // 인접한 학생들에게만 체육복을 빌려줄 수 있음
-    // 전체 n 을 돌면서 도난당한 학생들의 번호의 인접한 애들 중 여벌 체육복이 있으면 빼기
-    lost.sort((a,b) => a-b);
-    reserve.sort((a,b) => a-b);
-    
-    const indexFilter = (array, target) => {
+    const filterTarget = (array, target) => {
         return array.filter((value) => !(value === target))    
     }
     
     lost.forEach((i) => {
         if(lost.includes(i) && reserve.includes(i)) {
-            reserve = indexFilter(reserve, i);
-            lost = indexFilter(lost, i);
+            reserve = filterTarget(reserve, i);
+            lost = filterTarget(lost, i);
     }})
     
     for(let i = 1; i <= n ; i +=1) {
         if(!lost.includes(i)) continue;        
         
         if(reserve.includes(i-1)) {
-            reserve = indexFilter(reserve, i-1);
-            lost = indexFilter(lost, i);
+            reserve = filterTarget(reserve, i-1);
+            lost = filterTarget(lost, i);
             continue;
         }
         
         if(reserve.includes(i+1)) {
-            reserve = indexFilter(reserve, i+1);
-            lost = indexFilter(lost, i);
+            reserve = filterTarget(reserve, i+1);
+            lost = filterTarget(lost, i);
         }
     }
     
