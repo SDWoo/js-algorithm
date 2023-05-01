1b409b190153a68d08a5d33bd93af179a2d611ff
sdwoo <damjang67@naver.com>
2023-05-01 12:01:56 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1328 ms, Memory: 159256 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -0,0 +1,40 @@
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const N = parseInt(input[0]);
+const A = input[1].split(' ').map(Number).sort((a,b) => a-b);
+const M = parseInt(input[2]);
+const B = input[3].split(' ').map(Number);
+
+const answer = [];
+
+const getLowestIndex = (target, start, end) => {
+  while (start < end) {
+    let mid = parseInt((start + end) / 2);
+    if (A[mid] >= target) {
+      end = mid;
+    } else start = mid + 1;
+  }
+  return end;
+};
+
+const getHighestIndex = (target, start, end) => {
+  while (start < end) {
+    let mid = parseInt((start + end) / 2);
+    if (A[mid] > target) {
+      end = mid;
+    } else start = mid + 1;
+  }
+  return end;
+};
+
+for(let i = 0; i < M; i +=1) {
+    let high = getHighestIndex(B[i], 0, N);
+    let low = getLowestIndex(B[i], 0, N)
+  answer.push( high-low );
+}
+
+console.log(answer.join(' '));
